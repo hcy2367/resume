0df4123d86e9ab85a9e4e84e16ac6719ee4f4647
hcy2367 <hcy2367@163.com>
2013-10-01 18:48:09 +0800

    添加脚本

---
+++ js/index.js
@@ -0,0 +1,140 @@
+/*
+* update by hcy
+* 2013-10-1
+*/
+$(function() {
+	$(window).load(function() {
+		show();
+		animAuto(); // 自动播放动画
+	});
+	function show() {
+        $(".loading").fadeOut(); // 隐藏loading加载符
+        $(".container").fadeIn(); // 显示container模块   
+    }
+   
+    // 异步执行动画
+	var Queue = function() {
+		this.list = []; // 队列数组
+	};
+	Queue.prototype = {
+		// 添加回调函数到队列中
+		queue: function(fn) {
+			this.list.push(fn);
+			return this;
+		},
+		// 队列中每个函数执行的时间间隔
+		wait: function(num) {
+			this.list.push(num);
+			return this;
+		},
+		// 执行队列中的每个函数(根据给定时间间隔)
+		dequeue: function() {
+			var self = this,
+				list = self.list;
+			var item = list.shift() || function() {}; 
+			if (typeof item === "number") {
+				setTimeout(function() {
+					self.dequeue();
+				}, item);
+			} else if (typeof item === "function") {
+				item.call(self);
+				if (list.length) {
+					self.dequeue();
+				}
+			}
+		}
+	};
+	
+	// 自动播放动画
+	function animAuto() {
+		var q = new Queue(); // 实例化对象
+		q.queue(function() {
+			$(".animElem1").animate({top: "170px"}, 1000, function() {
+            	$(this).addClass("rotate10");
+            });
+		}).wait(1000)
+		.queue(function() {
+			$(".animElem2").animate({top: "170px"}, 1000, function() {
+            	$(this).addClass("rotate-10");
+            });
+		}).wait(1000)
+		.queue(function() {
+			$(".animElem3").animate({top: "170px"}, 1000, function() {
+            	$(this).addClass("rotate10");
+            });
+		}).wait(1000)
+		.queue(function() {
+			$(".animElem4").animate({top: "170px"}, 1000, function() {
+            	$(this).addClass("rotate-10");
+            });
+		}).wait(1000)
+		.queue(function() {
+			$(".animElem5").animate({top: "170px"}, 1000, function() {
+            	$(this).addClass("rotate10");
+            });
+		}).wait(1000)
+		.queue(function() {
+			$(".hello h3").fadeIn("slow");
+		}).wait(1000)
+		.queue(function() {
+			$(".hello").find(".hcy").fadeIn("slow").end().children(".nextBtn").show(1000);
+		}).dequeue();
+    
+    }
+
+
+    // 下一页(根据window的垂直滚动距离来显示元素)
+    $(window).scroll(function(event) {
+    	var curScrollTop = $(this).scrollTop();
+    	if (curScrollTop >= 750 && curScrollTop < 1450) {
+        	if ($(".about").css("display") === "none") {
+        		$(".about").fadeIn(); // 显示关于模块
+        		//console.log("1"); // 确保只执行一次代码
+        	}	
+        } else if (curScrollTop >= 1450 && curScrollTop < 2500) {
+        	if ($(".experience").css("display") === "none") {
+        		$(".experience").fadeIn(); // 显示联系模块
+        	}
+        } else if (curScrollTop >= 2500) {
+        	if ($(".contact").css("display") === "none") {
+        		$(".contact").fadeIn(); // 显示经验模块
+        	}
+        }
+        // 底栏固定
+        if (curScrollTop >= 3000) {
+        	$(".footer").fadeIn();
+        } else {
+        	$(".footer").fadeOut();
+        }
+    });
+    
+    $(".hello .nextBtn").click(function(event) {
+    	$("body, html").animate({scrollTop: "1000px"}, 1000, function() {
+    		$(".about").fadeIn(2000); // 显示关于模块
+    	});
+    });
+    $(".about .nextBtn").click(function(event) {
+    	$("body, html").animate({scrollTop: "1800px"}, 1000, function() {
+    		$(".experience").fadeIn(2000); // 显示联系模块
+    	});
+    });
+    $(".experience .nextBtn").click(function(event) {
+    	$("body, html").animate({scrollTop: "3000px"}, 1000, function() {
+    		$(".contact").fadeIn(2000); // 显示联系模块
+    	});
+    });
+
+
+    // 返回顶部按钮
+    $(window).scroll(function(){
+	    if ($(window).scrollTop() >= 500){
+            $(".returnBtn").fadeIn();
+        } else {
+            $(".returnBtn").fadeOut();
+	    }
+    });
+   $(".returnBtn").click(function(){
+        $("body, html").animate({scrollTop: 0}, 500);
+   });
+
+});
