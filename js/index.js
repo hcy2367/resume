3252ecc8d534feac67f866c96202bd335969b611
hcy2367 <hcy2367@163.com>
2013-10-03 23:05:34 +0800

    修改和更新脚本

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 /*
 * update by hcy
-* 2013-10-1
+* 2013-10-3
 */
 $(function() {
 	$(window).load(function() {
 		show();
+		countLeft(); // 计算动画元素left值
 		animAuto(); // 自动播放动画
 	});
 	function show() {
@@ -45,31 +46,54 @@ $(function() {
 		}
 	};
 	
+	var isResponsive = false; // 是否应用响应式
+	if ($(window).width() < 460) {
+		isResponsive = true;
+	}
+
+	// 计算动画元素的left值
+	function countLeft() {
+		var winWidth = $(window).width(),
+			helloWidth = $(".hello").width(),
+			elemWidth = $(".animElem1").width(), 
+			left1 = (winWidth - helloWidth) / 2,
+			left2 = left1 + elemWidth,
+			left3 = left2 + elemWidth,
+			left4 = left3 + elemWidth,
+			left5 = left4 + elemWidth;
+		// 设置left属性
+		$(".animElem1").css("left", left1);
+		$(".animElem2").css("left", left2);
+		$(".animElem3").css("left", left3);
+		$(".animElem4").css("left", left4);
+		$(".animElem5").css("left", left5);
+	}
+
 	// 自动播放动画
 	function animAuto() {
 		var q = new Queue(); // 实例化对象
 		q.queue(function() {
-			$(".animElem1").animate({top: "170px"}, 1000, function() {
+			$(".animElem1").animate({top: isResponsive === false ? "170px" : "110px"}, 1000, function() {
             	$(this).addClass("rotate10");
             });
 		}).wait(1000)
 		.queue(function() {
-			$(".animElem2").animate({top: "170px"}, 1000, function() {
+			$(".animElem2").animate({top: isResponsive === false ? "170px" : "110px"}, 1000, function() {
             	$(this).addClass("rotate-10");
             });
 		}).wait(1000)
 		.queue(function() {
-			$(".animElem3").animate({top: "170px"}, 1000, function() {
+			$(".animElem3").animate({top: isResponsive === false ? "170px" : "110px"}, 1000, function() {
             	$(this).addClass("rotate10");
             });
 		}).wait(1000)
 		.queue(function() {
-			$(".animElem4").animate({top: "170px"}, 1000, function() {
+			$(".animElem4").animate({top: isResponsive === false ? "170px" : "110px"}, 1000, function() {
             	$(this).addClass("rotate-10");
             });
 		}).wait(1000)
 		.queue(function() {
-			$(".animElem5").animate({top: "170px"}, 1000, function() {
+			$(".animElem5").animate({top: isResponsive === false ? "170px" : "110px"}, 1000, function() {
             	$(this).addClass("rotate10");
             });
 		}).wait(1000)
@@ -124,6 +148,18 @@ $(function() {
     	});
     });
 
+    // 经验模块
+    if (!isResponsive) {
+    	$(".experience").find("li").hover(function() {
+	    	$(this).addClass("current");
+	    }, function() {
+	    	$(this).removeClass("current");
+	    });
+    }
+    if (isResponsive) { // 设置宽度
+    	$(".contact").find("li:even").addClass("percent25").end().find("li:odd").addClass("percent75");
+
+    }
 
     // 返回顶部按钮
     $(window).scroll(function(){
